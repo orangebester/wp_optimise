3b9431083664f9368bb6674d2d58677e6e616bfe
Orangebe <orangebester@gmail.com>
2023-05-29 21:51:57 +0300

    Api post

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
 import requests
 import json
-import base64
 
-url = "https://left4write.home.blog/wp-json/wp/v2"
+# Set up the API URL and authentication
+url = 'https://dzygaspaw.com/wp-json/wp/v2/posts'
+user = 'Vitalii'
+password = '4BfN 6K4U KcJP nDlq NwEy Iidu'
+
+# Set the post data
+post_data = {
+    'title': 'First API Post',
+    'content': 'This is our first Python WordPress API post',
+    'status': 'publish'
+}
+
+# Create the headers for authentication
+headers = {
+    'Content-Type': 'application/json'
+}
+
+# Authenticate the request
+auth = (user, password)
+
+# Send the POST request to create the post
+response = requests.post(url, headers=headers, auth=auth, json=post_data)
+
+
+# Check the response
+if response.status_code == 201:
+    print('Post created successfully.')
+    print('Post ID:', response.json().get('id'))
+else:
+    print('Error creating post. Status Code:', response.status_code)
+    print('Error Message:', response.text)
